8b2f1fbecbc6d916df1f53c1f77a502b405d9aa9
sebasricop <sebastianrp21@hotmail.com>
2025-08-09 22:54:16 -0500

    Final archivo

--- 01_variables.php
+++ 01_variables.php
@@ -1,7 +1,33 @@
 <?php
-$nombre = "Juan";
-$edad = 30;
-$altura = 1.75;
+$course = "Curso de Sebas php";
+$author = "Guadalupe FEA";
+$precio = "Precio del siguiente curso: $2.500";
+$Date = date("d-m-Y");
 
-echo "Hola, mi nombre es $nombre, tengo $edad años y mido $altura metros.";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso de Sebas php</title>
+</head>
+
+<body>
+    <div class="course-container">
+        <h1>Bienvenido al <?= $course ?></h1>
+        <br>
+        <h2>Autor: <?= $author ?></h2>
+        <br>
+        <h3>Precio: <?= $precio ?></h3>
+        <br>
+        <h4>Fecha: <?= $Date ?></h4>
+        <br>
+    </div>
+
+    <P>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur totam, dolore velit exercitationem suscipit quia deleniti vitae dolorem amet eveniet eius perspiciatis nihil magnam repudiandae, enim, optio quisquam soluta officia.</P>
+</body>
+
+</html>
