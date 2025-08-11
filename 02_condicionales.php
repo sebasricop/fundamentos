edf6f76a32d2318bc365f461ea71e6cb814423ee
sebasricop <sebastianrp21@hotmail.com>
2025-08-10 20:46:39 -0500

    final condicionales

--- 02_condicionales.php
+++ 02_condicionales.php
@@ -2,7 +2,7 @@
 $course = "Curso de Sebas php";
 $archivado = true;
 $status = $archivado ? "archivado" : "activo";
-$nivelDelCurso = "intermedio";
+$nivelDelCurso = "avanzado";
 //Basico = "Recomendado para principiantes";
 //$intermedio = "Recomendado para quienes ya tienen conocimientos básicos";
 //$avanzado = "Recomendado para quienes desean profundizar en el tema";
@@ -25,7 +25,17 @@ $nivelDelCurso = "intermedio";
     </div>
     <P>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur totam, dolore velit exercitationem suscipit quia deleniti vitae dolorem amet eveniet eius perspiciatis nihil magnam repudiandae, enim, optio quisquam soluta officia.</P>
 
-    <P>Este curso está <?= $status ?></P>
+    <P>Nivel de curso <?= $nivelDelCurso ?></P>
+
+    <?php if ($nivelDelCurso == "basico") {
+        echo "<p>Recomendado para principiantes</p>";
+    } elseif ($nivelDelCurso == "intermedio") {
+        echo "<p>Recomendado para quienes ya tienen conocimientos básicos</p>";
+    } elseif ($nivelDelCurso == "avanzado") {
+        echo "<p>Recomendado para quienes desean profundizar en el tema</p>";
+    } ?>
+
 
 </body>
+
 </html>
