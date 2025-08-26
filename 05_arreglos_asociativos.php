9e7c497c37e2d79f489b1e1cc36163d329ce6c9f
sebasricop <sebastianrp21@hotmail.com>
2025-08-25 21:37:31 -0500

    05_arreglos_asociativos

---
+++ 05_arreglos_asociativos.php
@@ -0,0 +1,49 @@
+<?php
+
+$course = [
+    "title" => "curso de Lenguaje de programación",
+    "subtitle" => "Aprende desde cero",
+    "description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur totam.",
+    "tags" => [
+        "PHP",
+        "Laravel",
+        "JavaScript",
+        "HTML",
+        "CSS"
+    ],
+    'author' => "Sebastian",
+    'email' => "sebastian@ejemplo.com"
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $course['title'] ?></title>
+</head>
+
+<body>
+    <h1>Bienvenido al <?= $course['title'] ?></h1>
+
+    <h2><?= $course['subtitle'] ?></h2>
+
+    <P><?= $course['description'] ?></P>
+
+    <p>
+        <strong>Etiquetas</strong>
+    <ul>
+        <?php
+        //Recorrer el arreglo de etiquetas
+        foreach ($course['tags'] as $tag): ?>
+            <li><?= $tag ?></li>
+        <?php endforeach; ?>
+    </ul>
+    </p>
+
+</body>
+
+</html>
